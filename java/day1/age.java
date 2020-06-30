75444cd0148b2ddd4dbe82b19002c07daf529c23
STRATTON <strattonmmei8@gmail.com>
2020-06-30 20:05:48 +0300

    Initial commit

---
+++ java/day1/age.java
@@ -0,0 +1,7 @@
+class Age{
+	public static void main(String[] args) {
+
+		int myAge = 18;
+		System.out.println("I am " + myAge + " years old");
+	}
+}
---
+++ java/day2/sum.java
@@ -0,0 +1,11 @@
+class Sum{
+
+	public static void main(String[] args){
+
+		int a = 20;
+		int b = 30;
+		int sum = a + b;
+
+		System.out.println("The sum is ##" +sum);
+	}
+}
