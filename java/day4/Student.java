90465c36eccabb83b76cc7a6f8d3d4dab5680a25
STRATTON <strattonmmei8@gmail.com>
2020-07-03 22:35:43 +0300

    practice

---
+++ java/day4/Student.java
@@ -0,0 +1,22 @@
+class Student{
+
+	public static void main(String[] args){
+
+
+
+	String name = "John Doe";
+	String admissionNo = "sct298-2020";
+	String course = "ICT";
+	String gender = "Male";
+	String address = "Nairobi";
+	String sports = "Hockey";
+	String grade = "A";
+	int totalMarks = 489;
+	String room = "zoom111";
+	boolean activityFeePaid = true;
+	double feeBalance = 9000.0;
+
+	System.out.println("name: " +name+ "\nadmissionNo: " +admissionNo + "\ncourse: " +course+ "\ngender: " +gender+ "\naddress: " +address+ 
+		"\nsports:" +sports+ "\ngrade:" +grade+ "\ntotalMarks:" +totalMarks+ "\nroom:" +room+ "\nactivityFeePaid:"+activityFeePaid+ "\nfeeBalance: "+feeBalance);
+	}
+}
---
+++ java/day4/Sum3.java
@@ -0,0 +1,13 @@
+class Sum3{
+
+	public static void main(String[] args){
+
+		double a = 7.34;
+		double b = 2.946;
+		double sum = 0;
+
+		sum = a+b;
+
+		System.out.println(sum);
+	}
+}
